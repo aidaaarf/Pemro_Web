b1260c735823505b79d0e9af9f02281aaf5289b2
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-19 19:24:57 +0700

    Praktikum3_Soal3.2_operator

--- Pertemuan4/operator.php
+++ Pertemuan4/operator.php
@@ -9,10 +9,27 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a > $b;
+$hasilLebihBesar = $a < $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+
+echo "Operator Aritmatika <br><br>";
 echo "Hasil Tambah = {$hasilTambah} <br>";
 echo "Hasil Kurang = {$hasilKurang} <br>";
 echo "Hasil Kali = {$hasilKali} <br>";
 echo "Hasil Bagi = {$hasilBagi} <br>";
 echo "Hasil Sisa Bagi = {$sisaBagi} <br>";
 echo "Hasil Pangkat = {$pangkat} <br>";
+
+echo "<br><br>Operator Logika <br><br>";
+echo "Hasil Sama = {$hasilSama} <br>";
+echo "Hasil Tidak Sama = {$hasilTidakSama} <br>";
+echo "Hasil Lebih Kecil = {$hasilLebihKecil} <br>";
+echo "Hasil Lebih Besar = {$hasilLebihBesar} <br>";
+echo "Hasil Lebih Kecil Sama = {$hasilLebihKecilSama} <br>";
+echo "Hasil Lebih Besar Sama = {$hasilLebihBesarSama} <br>";
 ?>
