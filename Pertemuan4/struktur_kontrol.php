805b27539691b2ea0a46cb2d4d9b5a7d8ce80bf7
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-20 08:39:17 +0700

    Praktikum4_Soal4.4_struktur_kontrol_foreach

--- Pertemuan4/struktur_kontrol.php
+++ Pertemuan4/struktur_kontrol.php
@@ -33,4 +33,13 @@ for ($i = 1; $i <= $jumlahLahan ; $i++) {
     $jumlahBuah += ($tanamanPerlahan * $buahPerTanaman);
 }
 echo "<br><br>Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+//foreach
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+echo "<br><br>Total skor ujian adalah: $totalSkor";
 ?>
