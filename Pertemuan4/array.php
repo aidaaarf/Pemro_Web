5f4ee9274f5398ebcc93f73f9b6c4574d5928ac4
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-20 10:27:33 +0700

    Praktikum5_Soal5.4

--- Pertemuan4/array.php
+++ Pertemuan4/array.php
@@ -51,3 +51,21 @@ echo "<br><br>Daftar nilai Mahasiswa dalam mata kuliah $mataKuliah: <br>";
 foreach ($daftarNilai[$mataKuliah] as $nilai) {
     echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
 }
+
+// soal 5.4
+$nilaiMatematika = [
+    ['Alice', 85],
+    ['Bob', 92],
+    ['Charlie', 78],
+    ['David', 64],
+    ['Eva', 90],
+]; 
+ $nilaiRata = array_sum(array_column($nilaiMatematika,1)) / count($nilaiMatematika);
+ echo "<br><br>Nilai rata-rata: " . $nilaiRata;
+
+ echo "<br>Daftar siswa yang lulus: <br>";
+ foreach ($nilaiMatematika as $nilai) {
+    if ($nilai[1] > $nilaiRata) {
+        echo "Nama: {$nilai[0]} - Nilai: {$nilai[1]} <br>";
+    }
+ }
