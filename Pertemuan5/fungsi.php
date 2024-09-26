5a76b531137bfefb5f01fdcec2bd881a6962812b
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-26 13:34:30 +0700

    Soal_4_return

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -10,4 +10,11 @@ $saya = "Aida";
 $ucapanSalam = "Selamat Pagi";
 
 perkenalan($saya);
+echo "<hr>";
+
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+ echo "Umur saya adalah ". hitungUmur(2006, 2024) ." tahun"
 ?>
