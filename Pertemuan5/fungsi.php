488ec050379117b89366dadf576ec3c4aabe2721
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-26 13:24:04 +0700

    Soal_3_default

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -1,5 +1,5 @@
 <?php
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
@@ -9,5 +9,5 @@ echo "<hr>";
 $saya = "Aida";
 $ucapanSalam = "Selamat Pagi";
 
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
 ?>
