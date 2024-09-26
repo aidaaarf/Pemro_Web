0fad8a6a6bab393a2cc27e6adbbe4502903b7ee5
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-26 13:20:48 +0700

    Soal_2_parameter

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -1,9 +1,13 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Aida<br/>";
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
-perkenalan();
-perkenalan();
+perkenalan("Aida","Hallo");
+echo "<hr>";
+$saya = "Aida";
+$ucapanSalam = "Selamat Pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
