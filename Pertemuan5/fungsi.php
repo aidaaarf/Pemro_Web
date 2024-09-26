fff853e39e685457ec308deb98852a242f14853c
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-26 13:39:50 +0700

    Soal_5_fungsiDalamFungsi

--- Pertemuan5/fungsi.php
+++ Pertemuan5/fungsi.php
@@ -2,6 +2,7 @@
 function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Saya berusia ". hitungUmur(2006, 2024) ." tahun<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 perkenalan("Aida","Hallo");
