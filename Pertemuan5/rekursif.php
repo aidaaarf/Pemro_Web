6559f3eda8dd3d50fa589a4122564e08cc1fc7f6
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-26 14:08:48 +0700

    Soal_6_rekursif_angka

--- Pertemuan5/rekursif.php
+++ Pertemuan5/rekursif.php
@@ -3,4 +3,17 @@ function tampilkanHaloDunia(){
     echo "Halo dunia! <br>";
     tampilkanHaloDunia();
 }
-tampilkanHaloDunia();
+// tampilkanHaloDunia();
+
+for ($i=1; $i <= 25; $i++) { 
+  echo "Perulangan ke-{$i} <br>";
+}
+echo "<hr>";
+function tampilanAgka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+    if ($indeks < $jumlah) {
+        tampilanAgka($jumlah, $indeks + 1);
+    }
+}
+tampilanAgka(20);
+?>
