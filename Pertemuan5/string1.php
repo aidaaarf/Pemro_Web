f43da3f4c1c5ccf904531f81553bcc35cbc257d2
aidas777-ctrl <fadhila2903@gmail.com>
2024-09-26 19:31:58 +0700

    Soal_9_string

---
+++ Pertemuan5/string1.php
@@ -0,0 +1,13 @@
+<?php
+$loremIpsum = "Lorem ipsum dolor sit amet consectur Morbi posuere quis elit a consequat. Maecenas maximus,
+                urna ipsum finibus elit, quis venenatis metus Lorem vitae ante. Etiam ut purus sem. 
+                Ut efficitur lectus in ipsum tempor vehicula. Aliquam dapibus lacus sed ligula convallis
+                a congue quam maximus. Donec pulvinar est rutrum lacus feugiat.";
+
+echo "<p>{$loremIpsum}</p>";
+echo "Panjang karakter: " . strlen($loremIpsum) . "<br>";
+echo "Panjang Kata: " . str_word_count($loremIpsum) . "<br>";
+echo "<p>" . strtoupper($loremIpsum) . "</p>";
+echo "<p>" . strtolower($loremIpsum) . "</p";
+?>
+                
