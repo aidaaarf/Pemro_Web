e0691c19971c69e6665d56c4fbf5e0a5e68efc0e
aidas777-ctrl <fadhila2903@gmail.com>
2024-10-04 08:10:18 +0700

    Soal8_fungsiDalamFungsi

--- Pertemuan6/praktik_php/fungsi.php
+++ Pertemuan6/praktik_php/fungsi.php
@@ -2,17 +2,16 @@
 function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Saya berusia ". hitungUmur(2006, 2024) ." tahun<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 perkenalan("Aida","Hallo");
 echo "<hr>";
 $saya = "Aida";
 $ucapanSalam = "Selamat Pagi";
-
 perkenalan($saya);
 
 echo "<hr>";
-
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
