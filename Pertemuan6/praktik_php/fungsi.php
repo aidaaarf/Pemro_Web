e0179c7c675ad6e6964615672d440d91ca0a83c2
aidas777-ctrl <fadhila2903@gmail.com>
2024-10-04 07:54:58 +0700

    Soal4_fungsi

---
+++ Pertemuan6/praktik_php/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Aida<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+perkenalan();
+perkenalan();
+?>
