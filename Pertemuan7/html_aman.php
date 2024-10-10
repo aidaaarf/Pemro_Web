609b6dda32c7f75106f5062a3043f89157de363f
aidas777-ctrl <fadhila2903@gmail.com>
2024-10-10 23:10:15 +0700

    Praktikum4_Soal4.2_injection

--- Pertemuan7/html_aman.php
+++ Pertemuan7/html_aman.php
@@ -7,20 +7,39 @@
     <h2>Form Input PHP</h2>
 
     <?php
-    $input = "";
+    $input = $email = "";
+    $inputErr = $emailErr = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = $_POST['input'];
-        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8'); 
-        
-        
-        echo "<p>Data yang dimasukkan: " . $input . "</p>";
+        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');   
+    }else{
+        $inputErr = "Input tidak boleh kosong";
     }
+    if (!empty($_POST['email'])) {
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            
+        } else {
+            $emailErr = "Format email tidak valid";
+        }
+    } else {
+        $emailErr = "Email tidak boleh kosong";
+    }
+    
+    if (empty($inputErr) && empty($emailErr)) {
+            echo "<p>Data yang dimasukkan: " . $input . "</p>";
+            echo "<p>Email yang dimasukkan: " . $email . "</p>";
+        }
+
     ?>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
         <label for="input">Masukkan Data:</label>
         <input type="text" name="input" id="input" value="<?php echo $input; ?>"><br><br>
+
+        <label for="email">Masukkan Email:</label>
+        <input type="text" name="email" id="email" value="<?php echo $email; ?>">
         <input type="submit" name="submit" value="Submit">
     </form>
 </body>
