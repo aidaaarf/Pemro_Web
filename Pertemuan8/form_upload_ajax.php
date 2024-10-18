666d2abebf0e3ad7dad91fb6f24818bbfad72196
aidas777-ctrl <fadhila2903@gmail.com>
2024-10-18 08:15:26 +0700

    Praktikum3_Soal11

--- Pertemuan8/form_upload_ajax.php
+++ Pertemuan8/form_upload_ajax.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Unggah File Dokumen</title>
-    </head>
-        <body>
-            <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="file" id="file">
-            <input type="submit" name="submit" value="Unggah">
-            </form>
-            <div id="status"></div>
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-             <script src="upload.js"></script>
-        </body>
+<head>
+    <title>Unggah File Dokumen</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="upload.js"></script>
+</head>
+<body>
+    <h2>Upload File</h2>
+    <form id="upload-form" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file[]" multiple="multiple" accept=".jpg, .jpeg, .png">
+    <input type="submit" value="Upload">
+</form>
+    </form>
+    <div id="status"></div> 
+</body>
 </html>
--- Pertemuan8/upload_ajax.php
+++ Pertemuan8/upload_ajax.php
@@ -1,16 +1,18 @@
 <?php 
 if (isset($_FILES['file'])) {
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    $file_parts = explode('.', $_FILES['file']['name']);
-    $file_ext = strtolower(end($file_parts));
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $extensions = array("jpg", "jpeg", "png");
+
+    $totalFile = count($_FILES['file']['name']); 
+    for ($i = 0; $i < $totalFile; $i++) {
+        $file_name = $_FILES['file']['name'][$i];
+        $file_size = $_FILES['file']['size'][$i];
+        $file_tmp = $_FILES['file']['tmp_name'][$i];
+        $file_parts = explode('.', $file_name);
+        $file_ext = strtolower(end($file_parts));
 
     if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
+        $errors[] = "Ekstensi file yang diizinkan adalah JPG, JPEG,  atau PNG.";
     }
 
     if ($file_size > 2097152) {
@@ -19,9 +21,10 @@ if (isset($_FILES['file'])) {
 
     if (empty($errors) == true) {
         move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
+        echo "File  $file_name berhasil diunggah.<br>";
     } else {
         echo implode("", $errors);
     }
+} 
 }
 ?>
